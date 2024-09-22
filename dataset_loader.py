64e87c627953c247eb079cadc99d7b2d38fb04c3
dszvivian <dszvivian@gmail.com>
2024-09-23 01:26:07 +0530

    added variance, covariance and correlation Logic

---
+++ dataset_loader.py
@@ -0,0 +1,22 @@
+
+
+
+class dataset_loader:
+
+    def __init__(self,dataset) -> list:
+        self.dataset = dataset
+        return dataset
+    
+
+
+    #todo: Check wheather given list mathches with given value n 
+    def validate_dataset(
+            self,
+            n=2
+            ):
+        """
+        validates if it's a 2d array or not
+        """
+        return True
+
+        
