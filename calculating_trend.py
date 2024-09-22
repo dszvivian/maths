70205aca26bcafa68a1f06930019535ba619f646
dszvivian <dszvivian@gmail.com>
2024-09-22 19:57:47 +0530

    added basic structure of classes

---
+++ calculating_trend.py
@@ -0,0 +1,18 @@
+
+
+class calculate_trend:
+
+    def __init__(self,dataset:list[list[2]]) -> None:
+        print(dataset)
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    calculate_trend(
+        
+    )
+        
