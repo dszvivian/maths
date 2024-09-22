5cc2ce67edf5c9c797ab291e493e16a27999cdec
dszvivian <dszvivian@gmail.com>
2024-09-23 01:25:59 +0530

    added variance, covariance and correlation Logic

--- calculating_trend.py
+++ calculating_trend.py
@@ -1,3 +1,5 @@
+import math
+
 from utils import calculate_mean
 
 
@@ -6,43 +8,53 @@ class calculate_trend:
     def __init__(self,dataset:list[list[int]],point=(0,0)) -> None:
         
         self.dataset = dataset
-        self.R = self.correlation()
         self.n = len(dataset[0])
         self.point = (calculate_mean(dataset[0]),calculate_mean(dataset[1]))
-
-        print(self.point)
         
+        ((Vx,Vy),covariance) = self.summation_of_x_minus_xbar()
         
+        self.variance = (((Vx)/(self.n-1)),((Vy)/(self.n-1)))
 
+        self.covariance = covariance/(self.n-1)
 
-    def correlation(self):
-        pass
+        self.correlation = (self.covariance)/(math.sqrt(self.variance[0]) * math.sqrt(self.variance[1]))
 
-
-    def variance(dataset,mean):
+    def summation_of_x_minus_xbar(self):
         """
-        calcultes sum of all variance of all points 
-        returns (x,y)
+        Vx = ( x - xbar)
         """
 
-        pass
+        Vx, Vy,covariance = 0,0,0
 
+        meanx,meany = self.point
+
+        for numx,numy in zip(self.dataset[0],self.dataset[1]):
+            nx = (numx - meanx)
+            ny = (numy - meany)
+
+            Vx += (nx**2)
+            Vy += (ny**2)
+
+            covariance += (nx*ny)
+
+
+        return ((Vx,Vy),covariance)
     
-
     
-
-
 
 
 
 
 
 if __name__ == "__main__":
-    calculate_trend(
+    trend = calculate_trend(
         [
-            [1,2,3,4,5],
             [1,2,3,4,5],
             [1,2,3,4,5]
         ]
     ) 
+
+    print(f"Variance={trend.variance}")
+    print(f"Covariance={trend.covariance}")
+    print(f"Correlation={trend.correlation}")
         
