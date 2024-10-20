b8bfc6e01b9f9aeb4c1e3f87c51d7c4f6cf82869
dszvivian <dszvivian@gmail.com>
2024-10-20 21:16:29 +0530

    added basic skleton for Logistic regression

--- LogisticRegresssion.py
+++ LogisticRegresssion.py
@@ -76,12 +76,7 @@ class LogisticRegression:
 if __name__ == "__main__":
     obese_not_obese_dataset = DatasetLoader()
     obese_not_obese_dataset.csv_to_array(path="./datasets/obesity-classification-dataset.csv")
-    obese_not_obese_dataset.visualize_dataset()
-    
 
-    # Trying out Linear Regression for Logistic Regression 
-
-    regressor = LinearRegression()
+    regressor = LogisticRegression()
     regressor.train(obese_not_obese_dataset.X,obese_not_obese_dataset.Y)
-    regressor.plot()
     print(f"Actual value = {obese_not_obese_dataset.Y[-1]} Predicted Value = {regressor.predict(obese_not_obese_dataset.X[-1])}")
