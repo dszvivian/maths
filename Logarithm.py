cbc0d75e44582bb3676128ddab196100ee955832
dszvivian <dszvivian@gmail.com>
2024-11-05 07:58:11 +0530

    added log visualization

--- Logarithm.py
+++ Logarithm.py
@@ -2,9 +2,6 @@ from math import log
 import matplotlib.pyplot as plt
 
 
-from dataset_loader import DatasetLoader
-
-
 
 
 
@@ -21,14 +18,21 @@ if __name__ == "__main__":
     x = [i for i in range(1,100)]
     y = [i for i in range(1,100)]
 
+    logy = [ log(i) for i in y ]
 
-    normal_dataset = DatasetLoader(X=x, Y=y)
-    normal_dataset.visualize_dataset(title="Normal Dataset")
+    plt.xlabel("X")
+    plt.ylabel("Y")
 
-    y = [ log(i) for i in y ]
+    plt.axis('equal')
 
-    logarithmic_dataset = DatasetLoader(X=x, Y=y)
-    logarithmic_dataset.visualize_dataset(title="Logarithmic Dataset")
+    negative_log = [ -log(i) for i in y]
+
+    plt.plot(x,y)
+    plt.plot(x,logy)
+    plt.plot(x,negative_log)
+
+
+    plt.show()
     
 
 
